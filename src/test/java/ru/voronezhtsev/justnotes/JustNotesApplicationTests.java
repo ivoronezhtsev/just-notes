c968c4fcb11e104d292a8ce0d9473b6db525c823
igor <i.voronezhtsev@gmail.com>
2024-02-15 01:07:18 +0300

    initial commit

---
+++ src/test/java/ru/voronezhtsev/justnotes/JustNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.voronezhtsev.justnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
