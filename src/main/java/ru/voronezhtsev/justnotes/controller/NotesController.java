b405eb95ee453c1ceaf48dc3a5be6047c39df20d
igor <i.voronezhtsev@gmail.com>
2024-02-17 20:29:31 +0300

    Оптимизация импортов

--- src/main/java/ru/voronezhtsev/justnotes/controller/NotesController.java
+++ src/main/java/ru/voronezhtsev/justnotes/controller/NotesController.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import ru.voronezhtsev.justnotes.entity.Note;
 import ru.voronezhtsev.justnotes.service.NotesService;
 
