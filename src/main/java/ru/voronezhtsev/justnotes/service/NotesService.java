713e2e30998661eeab1c280a447ed3f70a733eb1
igor <i.voronezhtsev@gmail.com>
2024-02-19 23:50:56 +0300

    Убрал spring-security

--- src/main/java/ru/voronezhtsev/justnotes/service/NotesService.java
+++ src/main/java/ru/voronezhtsev/justnotes/service/NotesService.java
@@ -1,16 +1,11 @@
 package ru.voronezhtsev.justnotes.service;
 
 import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.ast.Not;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import ru.voronezhtsev.justnotes.controller.NoteModel;
+import ru.voronezhtsev.justnotes.entity.Note;
 import ru.voronezhtsev.justnotes.entity.User;
 import ru.voronezhtsev.justnotes.repository.NotesRepository;
-import ru.voronezhtsev.justnotes.entity.Note;
-import ru.voronezhtsev.justnotes.repository.UsersRepository;
-import ru.voronezhtsev.justnotes.security.AppUserPrincipal;
 
 import java.util.List;
 
@@ -21,18 +16,18 @@ public class NotesService {
     private final NotesRepository notesRepository;
 
     public List<Note> findNotes() {
-        return notesRepository.findByUserId(getCurrentUser().getUserId());
+        return notesRepository.findByUserId(1);
     }
 
     public void save(NoteModel noteModel) {
-        Note note = new Note(new User(getCurrentUser().getUserId()), noteModel.getTitle(), noteModel.getContent());
+        Note note = new Note(new User(1L), noteModel.getTitle(), noteModel.getContent());
         notesRepository.save(note);
     }
 
-    private AppUserPrincipal getCurrentUser() {
+    /*private AppUserPrincipal getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return (AppUserPrincipal) authentication.getPrincipal();
-    }
+    }*/
 
     public void delete(long noteId) {
         notesRepository.deleteById(noteId);
