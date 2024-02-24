0b366e9c8a8d152993173f1947b45695571b8ece
igor <i.voronezhtsev@gmail.com>
2024-02-24 15:36:44 +0300

    Убрал неиспользуемые импорты

--- src/main/java/ru/voronezhtsev/justnotes/service/NotesService.java
+++ src/main/java/ru/voronezhtsev/justnotes/service/NotesService.java
@@ -1,15 +1,13 @@
 package ru.voronezhtsev.justnotes.service;
 
 import lombok.RequiredArgsConstructor;
-import org.aspectj.weaver.ast.Not;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import ru.voronezhtsev.justnotes.controller.NoteModel;
+import ru.voronezhtsev.justnotes.entity.Note;
 import ru.voronezhtsev.justnotes.entity.User;
 import ru.voronezhtsev.justnotes.repository.NotesRepository;
-import ru.voronezhtsev.justnotes.entity.Note;
-import ru.voronezhtsev.justnotes.repository.UsersRepository;
 import ru.voronezhtsev.justnotes.security.AppUserPrincipal;
 
 import java.util.List;
