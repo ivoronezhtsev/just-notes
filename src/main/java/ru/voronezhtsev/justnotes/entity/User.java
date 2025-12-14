67a794cdc4a002da6e95ceed31da97890b18cbe7
Igor Voronezhtsev <i.voronezhtsev@gmail.com>
2025-12-14 21:38:22 +0300

    Рабочая версия с postgresql и materialize

--- src/main/java/ru/voronezhtsev/justnotes/entity/User.java
+++ src/main/java/ru/voronezhtsev/justnotes/entity/User.java
@@ -1,6 +1,7 @@
 package ru.voronezhtsev.justnotes.entity;
 
 import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,6 +14,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@EqualsAndHashCode
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
